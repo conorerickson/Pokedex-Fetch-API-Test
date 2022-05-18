f29404f1b26fb1a337b02fceb3750a76f5bd397c
Conor Erickson <89137464+conorerickson@users.noreply.github.com>
2022-05-18 17:06:31 -0400

    Initial Commit

---
+++ scripts/app.js
@@ -0,0 +1,37 @@
+const pokedex = document.querySelector('#pokedex');
+
+const fetchPokemon = () => {
+    const promises = [];
+    for (let i = 1; i <= 151; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then((results) => {
+        const pkmn = results.map((result) => ({
+            name: result.name,
+            image: result.sprites['front_default'],
+            type: result.types.map((type) => type.type.name).join(', '),
+            id: result.id
+        }));
+        displayPkmn(pkmn);
+    });
+};
+
+const displayPkmn = (pkmn) => {
+    console.log(pkmn);
+    const pokemonHTML = pkmn
+        .map(
+            (pokeman) => `
+        <li class="card">
+            <img class="card-image" src="${pokeman.image}"/>
+            <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+            <p class="card-subtitle">Type: ${pokeman.type}</p>
+        </li>
+    `
+        )
+        .join('');
+    pokedex.innerHTML = pokemonHTML;
+};
+fetchPokemon();
+
+
